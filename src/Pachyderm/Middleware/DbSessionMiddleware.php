f582dde7f61d571ee462d042b40671ca32373c3b
Timothé Mermet-Buffet <tim@aliengen.com>
2022-04-24 17:53:23 +0800

    Fix issue when db doesn't connect in the middleware.

--- src/Pachyderm/Middleware/DbSessionMiddleware.php
+++ src/Pachyderm/Middleware/DbSessionMiddleware.php
@@ -5,26 +5,32 @@ namespace Pachyderm\Middleware;
 use Pachyderm\Middleware\MiddlewareInterface;
 use Pachyderm\Db;
 
-class DbSessionMiddleware implements MiddlewareInterface {
-    public function __construct() {}
+class DbSessionMiddleware implements MiddlewareInterface
+{
+    public function __construct()
+    {
+    }
 
-    public function handle(\Closure $next) {
+    public function handle(\Closure $next)
+    {
         try {
             $db = Db::getInstance()->mysql();
             $db->begin_transaction();
 
             $response = $next();
 
-            if($response[0] < 300) {
+            if ($response[0] < 300) {
                 $db->commit();
             } else {
                 $db->rollBack();
             }
 
             return $response;
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             $response = [500, ['error' => $e->getMessage()]];
-            $db->rollBack();
+            if (!empty($db)) {
+                $db->rollBack();
+            }
             return $response;
         }
     }
