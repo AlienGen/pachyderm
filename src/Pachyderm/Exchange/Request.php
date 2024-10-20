83a22d0f34213ba68d4f2ab5acab12225ba49d5d
Timothé Mermet-Buffet <tim@aliengen.com>
2024-10-20 17:45:28 +0800

    Remove unnecessary property.

--- src/Pachyderm/Exchange/Request.php
+++ src/Pachyderm/Exchange/Request.php
@@ -6,27 +6,15 @@ use Pachyderm\Utils\IterableObjectSet;
 
 class Request extends IterableObjectSet
 {
-    private $body;
-
     /**
      * @param mixed $body
      */
     public function __construct(mixed $body)
     {
-        $this->body = $body;
+        $this->_data = $body;
     }
 
     public function body(): mixed {
-        return $this->body;
-    }
-
-    public function __get($key)
-    {
-        return $this->body[$key];
-    }
-
-    public function __set($key, $value)
-    {
-        $this->body[$key] = $value;
+        return $this->_data;
     }
 }
